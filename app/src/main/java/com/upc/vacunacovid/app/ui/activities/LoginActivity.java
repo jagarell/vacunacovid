e10f3b8b4914c07195d77482868fdc4169c0aaec
José Arellano <arellano0217@gmail.com>
2021-04-22 18:45:24 -0500

    Se agrega  LoginActivity

--- app/src/main/java/com/upc/vacunacovid/app/ui/activities/LoginActivity.java
+++ app/src/main/java/com/upc/vacunacovid/app/ui/activities/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.upc.vacunacovid;
+package com.upc.vacunacovid.app.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.upc.vacunacovid.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/upc/vacunacovid/app/ui/activities/MainActivity.java
@@ -0,0 +1,20 @@
+package com.upc.vacunacovid.app.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.upc.vacunacovid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
